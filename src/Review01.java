ecef4c5bbc4c24487f4c35f2c09fa96e5743bccd
Akane Oba <aka1105maron@gmail.com>
2024-01-10 19:17:02 +0900

    fix method name

--- src/Review01.java
+++ src/Review01.java
@@ -4,11 +4,11 @@ public class Review01 {
     public static void main(String[] args) {
         int withoutTax = 1500;
         int taxRate = 10;
-        int tax = getTax(withoutTax, taxRate);
+        int tax = tax(withoutTax, taxRate);
         int taxIncluded = withoutTax + tax;
         System.out.println(withoutTax + "円の商品の税込価格は" + taxIncluded + "円（消費税は" + tax + "円）です。");
     }
-    public static int getTax(int withoutTax, int taxRate) {
+    public static int tax(int withoutTax, int taxRate) {
         int tax = (int)(withoutTax * (double)(taxRate*0.01));
         return tax;
     }
